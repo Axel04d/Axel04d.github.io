5ba267157b4136abc63352793e6db43d4ebd4c3d
Axel <159165656+Axel04d@users.noreply.github.com>
2024-10-04 00:05:02 -0600

    Develop (#33)
    * create develop branch1
    
    * create header branch #1
    
    * add work_experience branch #2
    
    * add education branch #3
    
    * add karma.conf
    
    * Create develop_CI_CD.yml
    
    * Update develop_CI_CD.yml (#22)
    
    * add skills branch #4 (#23)
    
    create a skills branch
    
    * add languajes branch #5 (#24)
    
    branch langujes
    
    * add interest branch #6 (#25)
    
    branch interest
    
    * add achivaments branch #7 (#26)
    
    add branch achivements

---
+++ src/app/achivaments/achivaments.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-achivaments',
+  standalone: true,
+  imports: [],
+  templateUrl: './achivaments.component.html',
+  styleUrl: './achivaments.component.css'
+})
+export class AchivamentsComponent {
+
+}
